8b0c0c705bdd075e0219449447241f03d7cdc825
Frank Tang <ftang@chromium.org>
2019-03-26 02:42:00 -0700

    Add filter files for cast, android, ios and flutter
    Data Size
     7037760 android/icudtl.dat
     5920512 cast/icudtl.dat
    10837984 chromeos/icudtl.dat
    10837984 common/icudtb.dat
    10837984 common/icudtl.dat
     1228384 flutter/icudtl.dat
     7001328 ios/icudtl.dat
    
    Bug: chromium:943348
    Change-Id: I39cf17574f83e8371de7a1cd8797fa928270af46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/deps/icu/+/1539143
    Reviewed-by: Jungshik Shin <jshin@chromium.org>

--- scripts/make_data_all.sh
+++ scripts/make_data_all.sh
@@ -4,22 +4,45 @@ set -x
 
 ICUROOT="$(dirname "$0")/.."
 
+echo "Build the necessary tools"
 "${ICUROOT}/source/runConfigureICU" --enable-debug --disable-release Linux/gcc --disable-tests
+make -j 120
+
+echo "Build the filtered data for common"
 (cd data && make clean)
 $ICUROOT/scripts/config_data.sh common
 make -j 120
 $ICUROOT/scripts/copy_data.sh common
 
-#$ICUROOT/cast/patch_locale.sh
-#$ICUROOT/scripts/make_data.sh
+echo "Build the filtered data for chromeos"
+# For now just copy the build result from common
+$ICUROOT/scripts/copy_data.sh chromeos
+
+echo "Build the filtered data for Cast"
+(cd data && make clean)
+$ICUROOT/scripts/config_data.sh cast
+$ICUROOT/cast/patch_locale.sh
+make -j 120
 $ICUROOT/scripts/copy_data.sh cast
-#$ICUROOT/android/patch_locale.sh
-#$ICUROOT/scripts/make_data.sh
+
+echo "Build the filtered data for Android"
+(cd data && make clean)
+$ICUROOT/scripts/config_data.sh android
+make -j 120
 $ICUROOT/scripts/copy_data.sh android
-#$ICUROOT/ios/patch_locale.sh
-#$ICUROOT/scripts/make_data.sh
+
+echo "Build the filtered data for iOS"
+(cd data && make clean)
+$ICUROOT/scripts/config_data.sh ios
+make -j 120
 $ICUROOT/scripts/copy_data.sh ios
-#$ICUROOT/flutter/patch_brkitr.sh
-#$ICUROOT/scripts/make_data.sh
+
+echo "Build the filtered data for Flutter"
+(cd data && make clean)
+$ICUROOT/scripts/config_data.sh flutter
+$ICUROOT/flutter/patch_brkitr.sh
+make -j 120
 $ICUROOT/scripts/copy_data.sh flutter
+
+echo "Clean up the git"
 $ICUROOT/scripts/clean_up_data_source.sh
